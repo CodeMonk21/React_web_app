52cbe179bf55a5402a3988521fb276ddc1857392
CodeMonk21 <jagritmadaan2001@gmail.com>
2024-04-12 15:38:15 +0530

    add routing and  pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
+import {Routes, Route } from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Faq from './pages/Faq';
+import Product from './pages/Product';
 
 function App() {
   return (
     <>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/product' element={<Product />} />
+        <Route path='/faq' element={<Faq />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+
+      </Routes>
     </>
-    
+
   );
 }
 
