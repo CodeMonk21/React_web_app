1ec59695ef7bb8dc172ae6a83cd833d935cab8be
CodeMonk21 <jagritmadaan2001@gmail.com>
2024-04-18 17:47:16 +0530

    about page done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,28 +5,28 @@ import { Link } from 'react-router-dom'
 function Navbar() {
     return (
         <>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary" style={{backgroundColor:"#124076"}}>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary" style={{ backgroundColor: "#124076" }}>
                 <div className="container-fluid" >
-                    <Link className="navbar-brand" to="/" style={{color:"#E6E7E9"}}>SensorSphere</Link>
+                    <Link className="navbar-brand" to="/" style={{ color: "#E6E7E9" }}>SensorSphere</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse d-flex justify-content-center" id="navbarSupportedContent">
                         <ul className="navbar-nav  mb-2 mb-lg-0 ">
                             <li className="nav-item Navbar-link">
-                                <Link className="nav-link active" aria-current="page" to="/" style={{color:"#E6E7E9"}}>Home</Link>
+                                <Link className="nav-link active" aria-current="page" to="/" style={{ color: "#E6E7E9" }}>Home</Link>
                             </li>
                             <li className="nav-item Navbar-link">
-                                <Link className="nav-link" to="product" style={{color:"#E6E7E9"}}>Products</Link>
+                                <Link className="nav-link" to="about" style={{ color: "#E6E7E9" }}>About Us</Link>
                             </li>
                             <li className="nav-item Navbar-link">
-                                <Link className="nav-link" to="faq" style={{color:"#E6E7E9"}}>FAQ's</Link>
+                                <Link className="nav-link" to="product" style={{ color: "#E6E7E9" }}>Products</Link>
                             </li>
                             <li className="nav-item Navbar-link">
-                                <Link className="nav-link" to="about" style={{color:"#E6E7E9"}}>About Us</Link>
+                                <Link className="nav-link" to="faq" style={{ color: "#E6E7E9" }}>FAQ's</Link>
                             </li>
                             <li className="nav-item Navbar-link">
-                                <Link className="nav-link" to="contact" style={{color:"#E6E7E9"}}>Contact Us</Link>
+                                <Link className="nav-link" to="contact" style={{ color: "#E6E7E9" }}>Contact Us</Link>
                             </li>
                         </ul>
                     </div>
